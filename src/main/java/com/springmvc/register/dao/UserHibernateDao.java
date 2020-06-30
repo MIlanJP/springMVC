002fcf0e6d5d8dd6dc63f794f6fd9dd6db1e71c6
kyoolbaba <kyoolbaba@gmail.com>
2020-07-01 01:22:08 +0530

    Added UserHIbernateDao File and shifted CRUD operations from TestHIbernateConfig

--- src/main/java/com/springmvc/register/dao/UserHibernateDao.java
+++ src/main/java/com/springmvc/register/dao/UserHibernateDao.java
@@ -6,15 +6,21 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public class UserHibernateDao {
 
     SessionFactory sessionFactory=new Configuration().configure().
             addAnnotatedClass(HUser.class).buildSessionFactory();
+    Transaction transaction;
 
     public void insertUser(HUser user){
+        transaction=null;
         Session session=sessionFactory.getCurrentSession();
-        Transaction transaction=session.beginTransaction();
+         transaction=session.beginTransaction();
         try {
             session.save(user);
             transaction.commit();
@@ -25,4 +31,68 @@ public class UserHibernateDao {
             session.close();
         }
     }
+
+    private void readObject() {
+        transaction=null;
+        Session session=sessionFactory.getCurrentSession();
+        transaction=session.beginTransaction();
+        try {
+            HUser user = session.get(HUser.class, 1);
+            System.out.println(user);
+            transaction.commit();
+        }catch(HibernateException e){
+            if(transaction!=null)transaction.rollback();
+        }finally{
+            session.close();
+        }
+    }
+
+    public void update(String username,String lastname){
+        transaction=null;
+        Session session=sessionFactory.getCurrentSession();
+        transaction=session.beginTransaction();
+        try {
+            HUser user = (HUser) session.createQuery("from HUser s where s.username='" + username + "'").list().get(0);
+            user.setLastName(lastname);
+            transaction.commit();
+        }catch (HibernateException e){
+            if(transaction!=null)transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+    }
+
+    public void delete(String username){
+        transaction=null;
+//                Creating session
+        Session session=sessionFactory.getCurrentSession();
+        transaction=session.beginTransaction();
+//        Creating Object
+        try{
+            HUser user = (HUser) session.createQuery("from HUser s where s.username='" + username + "'").list().get(0);
+            session.delete(user);
+            transaction.commit();
+        }catch(HibernateException e){
+            if(transaction!=null) transaction.rollback();
+            e.printStackTrace();
+        }finally{
+            session.close();
+        }
+    }
+
+    public void query(String username){
+        transaction=null;
+        Session session=sessionFactory.getCurrentSession();
+        transaction=session.beginTransaction();
+        try {
+            List<HUser> listOfUsers = session.createQuery("from HUser s where s.username='" + username + "'").list();
+            System.out.println(listOfUsers.get(0));
+            transaction.commit();
+        }catch(HibernateException e){
+            if(transaction!=null)transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/main/java/com/springmvc/register/services/HibernateUserService.java
@@ -0,0 +1,25 @@
+package com.springmvc.register.services;
+
+import com.springmvc.register.dao.UserHibernateDao;
+import com.springmvc.register.model.HUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HibernateUserService {
+
+    @Autowired
+    UserHibernateDao userHibernateDao;
+
+    public void insertUser(HUser user){
+        userHibernateDao.insertUser(user);
+    }
+
+
+
+    public void deleteUser(String username){
+        userHibernateDao.delete(username);
+    }
+
+
+}
